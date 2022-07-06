bd13d26b79bedd9acb0031bebacbf9ad4d215fbf
Julio Giovanni Flores Zermeño <jg250998@gmail.com>
2022-07-06 02:13:25 -0500

    addex index

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout';
+
+const index = () => {
+  return (
+    <Layout>
+        index
+    </Layout>
+  )
+}
+
+export default index
