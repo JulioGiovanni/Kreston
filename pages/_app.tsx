e9344d739cca0eff641f1bd2fad8f9a2e044bd10
Julio Giovanni Flores Zermeño <jg250998@gmail.com>
2022-08-20 03:57:14 -0500

    quitar fuentes cdn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,12 +31,12 @@ export default function App(props: Props) {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         {/* <link rel="shortcut icon" href="/favicon.svg" /> */}
         <link rel="shortcut icon" href="/images/KRESTON-CSM-LOGO.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Serif:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </Head>
       <SessionProvider>
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
